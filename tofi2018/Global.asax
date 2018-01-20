fca645041e2b8c9cb29997f34273ea7b8e8b7098
lizasixsixsix <lizasixsixsix@gmail.com>
2018-01-20 19:30:31 +0300

    Add project files.

---
+++ tofi2018/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tofi2018.MvcApplication" Language="C#" %>
---
+++ tofi2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tofi2018.Startup))]
+namespace tofi2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
