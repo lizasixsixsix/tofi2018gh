6c7c0ce7bb4e26c4a1c71fe3ab8c20ddea6bdad8
lizasixsixsix <lizasixsixsix@gmail.com>
2018-01-29 01:32:23 +0300

    file upload & download implemented

---
+++ tofi2018/Controllers/FileController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using tofi2018.Models;
+
+namespace tofi2018.Controllers
+{
+    public class FileController : Controller
+    {
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            string fileName = string.Empty;
+
+            if (file != null && file.ContentLength > 0)
+            {
+                fileName = Path.GetFileName(file.FileName);
+
+                string now = DateTime.Now.ToString("o");
+
+                var directory = Directory.CreateDirectory(
+                    Path.Combine(Server.MapPath("~/App_Data/Uploaded"),
+                                 DateTime.Now.ToString("yyyyMMddHHmmssffffff")));
+
+                var path = Path.Combine(directory.FullName, fileName);
+
+                file.SaveAs(path);
+
+                return RedirectToAction("Uploaded", new { file = fileName });
+            }
+
+            return RedirectToAction("NotUploaded");
+        }
+
+        public ActionResult Uploaded(string file)
+        {
+            ViewBag.FileName = file;
+
+            return View();
+        }
+
+        public ActionResult NotUploaded()
+        {
+            return View();
+        }
+
+        public ActionResult Download()
+        {
+            var dir = new DirectoryModel(Server.MapPath("~/App_Data/Uploaded/"));
+
+            return View(dir);
+        }
+
+        public ActionResult DownloadFile(string fileName)
+        {
+            string contentType = "application/pdf";
+
+            return File(fileName, contentType, Path.GetFileName(fileName));
+        }
+    }
+}
---
+++ tofi2018/Models/FileModels.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace tofi2018.Models
+{
+    public class FileUploadModel
+    {
+    }
+
+    public class DirectoryModel
+    {
+        public string Parent { get; private set; }
+
+        public Dictionary<string, List<string>> Folders { get; private set; }
+
+        public DirectoryModel(string parent)
+        {
+            this.Parent = parent;
+
+            this.Folders = new Dictionary<string, List<string>>();
+
+            foreach (string dir in Directory.GetDirectories(
+                         parent, "*", SearchOption.TopDirectoryOnly))
+            {
+                if (Directory.GetFiles(dir, "*").Length > 0)
+                {
+                    this.Folders.Add(dir, new List<string>());
+
+                    foreach (string file in Directory.GetFiles(
+                                 dir, "*", SearchOption.AllDirectories))
+                    {
+                        this.Folders[dir].Add(file);
+                    }
+                }
+            }
+        }
+    }
+}
